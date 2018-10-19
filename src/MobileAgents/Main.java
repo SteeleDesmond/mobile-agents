eba5b0c0c04801ea4c8b21c198441cdaa207f37b
Brandon D. Wade <bwade@cs.unm.edu>
2018-10-19 15:26:00 -0600

    Cleaned up the code, also update the agents and display classes threads to make the test animation run smoother.

--- src/MobileAgents/Main.java
+++ src/MobileAgents/Main.java
@@ -10,33 +10,24 @@ import MobileAgents.agents.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+
 public class Main extends Application
 {
 
-    BlockingQueue <Integer > agentsPosition;
 
     public Agent agent1;
 
     public Display sceneDisplay;
-
-    public Thread displayThread;
-
-    public Thread agentThread;
 
 
 
 
     public Main()
     {
-        agentsPosition = new LinkedBlockingQueue <>();
 
-        sceneDisplay = new Display(1000,1000, agentsPosition);
+        agent1 = new Agent("Agent1",0,1);
 
-        agent1 = new Agent("Agent1",0,agentsPosition);
-
-        displayThread = new Thread(sceneDisplay);
-
-        agentThread = new Thread(agent1);
+        sceneDisplay = new Display(1000,1000, agent1);
     }
 
     @Override
@@ -47,25 +38,16 @@ public class Main extends Application
 
         primaryStage.setScene(new Scene(new Group(sceneDisplay.root)));
 
+        agent1.start();
 
-         agentThread.start();
-         displayThread.start();
+        sceneDisplay.start();
 
         primaryStage.show();
-
-
-
-
-
     }
 
 
     public static void main(String[] args)
     {
-
         launch(args);
-
-
-
     }
 }
